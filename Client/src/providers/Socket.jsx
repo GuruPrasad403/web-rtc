0563470fce226542d13d2ecf3ceb5f516a5aa271
Guru Prasad G <128472613+GuruPrasad403@users.noreply.github.com>
2025-04-06 21:34:13 +0530

    Update Socket.jsx

--- Client/src/providers/Socket.jsx
+++ Client/src/providers/Socket.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 
 export default function SocketProvider({ children }) {
   const socket = io(
-    "https://0e6ea978-db8c-43b1-b068-436bc70be319-00-1cl2u3bwczal2.kirk.replit.dev/",
+    "https://web-rtc-mu-livid.vercel.app/",
   );
 
   return (
